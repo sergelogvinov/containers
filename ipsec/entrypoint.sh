136d2cbbbaeb0f96943ecef1fbd708ccc6c3dd65
Serge Logvinov <serge.logvinov@sinextra.dev>
2023-05-22 18:05:01 +0300

    add ipsec

---
+++ ipsec/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Update sysctl settings
+SYST='/sbin/sysctl -e -q -w'
+if [ "$(getconf LONG_BIT)" = "64" ]; then
+  SHM_MAX=68719476736
+  SHM_ALL=4294967296
+else
+  SHM_MAX=4294967295
+  SHM_ALL=268435456
+fi
+$SYST kernel.msgmnb=65536
+$SYST kernel.msgmax=65536
+$SYST kernel.shmmax=$SHM_MAX
+$SYST kernel.shmall=$SHM_ALL
+$SYST net.ipv4.conf.all.accept_source_route=0
+$SYST net.ipv4.conf.all.accept_redirects=0
+$SYST net.ipv4.conf.all.send_redirects=0
+$SYST net.ipv4.conf.all.rp_filter=0
+$SYST net.ipv4.conf.default.accept_source_route=0
+$SYST net.ipv4.conf.default.accept_redirects=0
+$SYST net.ipv4.conf.default.send_redirects=0
+$SYST net.ipv4.conf.default.rp_filter=0
+$SYST net.ipv4.conf.eth0.send_redirects=0
+$SYST net.ipv4.conf.eth0.rp_filter=0
+
+mkdir -p /run/pluto /var/run/pluto /var/lib/ipsec/nss
+ipsec initnss --nssdir /var/lib/ipsec/nss
+
+exec /usr/libexec/ipsec/pluto --nofork --nssdir /var/lib/ipsec/nss --config /etc/ipsec.conf --secretsfile /etc/ipsec.secrets --stderrlog
